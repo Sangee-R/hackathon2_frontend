331edca0a4b8c6a1d94d576a335aec7e64d8682f
Sangee-R <sangeethar0205@gmail.com>
2023-03-05 12:25:25 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+let moviesList = ['Squid Game', 'Money heist', 'All of us are Dead', 'The Pursuit of Happyness', 'Alice in Borderland'];
+let images=['images.jpg','images.jpg','images.jpg']
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.images=images;
+exports.slots = slots;
+exports.seats = seats;
